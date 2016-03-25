32f24d857c52544eb7f7c1b5de87068eb25675fb
Artoria <Artoria>
2016-03-25 08:58:13 +0800

    deleted:    com/M3L/arious/AndroidManifest.xml 	deleted:    com/M3L/arious/ant.properties 	deleted:    com/M3L/arious/bin/AndroidManifest.xml 	deleted:    com/M3L/arious/bin/AndroidManifest.xml.d 	deleted:    com/M3L/arious/bin/Arious-release-unaligned.apk 	deleted:    com/M3L/arious/bin/Arious-release-unsigned.apk 	deleted:    com/M3L/arious/bin/Arious-release-unsigned.apk.d 	deleted:    com/M3L/arious/bin/Arious-release.apk 	deleted:    com/M3L/arious/bin/Arious.ap_ 	deleted:    com/M3L/arious/bin/Arious.ap_.d 	deleted:    com/M3L/arious/bin/build.prop 	deleted:    com/M3L/arious/bin/classes.dex 	deleted:    com/M3L/arious/bin/classes.dex.d 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/Arious$1.class 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/Arious$10.class 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/Arious$11.class 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/Arious$12.class 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/Arious$13.class 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/Arious$14.class 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/Arious$15.class 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/Arious$16.class 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/Arious$17.class 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/Arious$18.class 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/Arious$19.class 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/Arious$2.class 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/Arious$20.class 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/Arious$21.class 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/Arious$22.class 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/Arious$3.class 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/Arious$4.class 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/Arious$5.class 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/Arious$6.class 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/Arious$7.class 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/Arious$8.class 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/Arious$9.class 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/Arious.class 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/BuildConfig.class 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/R$attr.class 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/R$id.class 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/R$layout.class 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/R$string.class 	deleted:    com/M3L/arious/bin/classes/com/M3L/arious/R.class 	deleted:    com/M3L/arious/bin/dexedLibs/annotations-ba6bf81547ff891870f6ac074531147e.jar 	deleted:    com/M3L/arious/bin/jarlist.cache 	deleted:    com/M3L/arious/bin/proguard.txt 	deleted:    com/M3L/arious/build.xml 	deleted:    com/M3L/arious/gen/R.java.d 	deleted:    com/M3L/arious/gen/com/M3L/arious/BuildConfig.java 	deleted:    com/M3L/arious/gen/com/M3L/arious/R.java 	deleted:    com/M3L/arious/local.properties 	deleted:    com/M3L/arious/main.keystore 	deleted:    com/M3L/arious/proguard-project.txt 	deleted:    com/M3L/arious/project.properties 	deleted:    com/M3L/arious/res/layout/main.xml 	deleted:    com/M3L/arious/res/values/strings.xml 	deleted:    com/M3L/arious/src/com/M3L/arious/Arious.java

--- com/M3L/arious/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-F:\Angen\git\Angen\com\M3L\arious\bin\AndroidManifest.xml : \
-F:\Angen\git\Angen\com\M3L\arious\AndroidManifest.xml \
--- com/M3L/arious/bin/Arious.ap_.d
+++
@@ -1,4 +0,0 @@
-F:\Angen\git\Angen\com\M3L\arious\bin\Arious.ap_ \
- : F:\Angen\git\Angen\com\M3L\arious\res\layout\main.xml \
-F:\Angen\git\Angen\com\M3L\arious\res\values\strings.xml \
-F:\Angen\git\Angen\com\M3L\arious\bin\AndroidManifest.xml \
